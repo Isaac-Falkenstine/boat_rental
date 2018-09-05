8f96426eacd12c28535ef3e7a8f4bf8941ac187f
Isaac Falkenstine <isaac@falkenstine.com>
2018-09-05 13:30:35 -0600

    Added test for updated dock class and started work on the methods to make the test pass

--- lib/dock.rb
+++ lib/dock.rb
@@ -5,5 +5,21 @@ class Dock
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+    @dock_hours = 0
+  end
+
+  def rent(boat, renter)
+
+  end
+
+  def return(boat)
+  end
+
+  def log_hour
+    @dock_hours += 1
+  end
+
+  def revenue
+    @dock_hours
   end
 end
