517c792890985746b7315cfb968d6ec27c6d2266
Isaac Falkenstine <isaac@falkenstine.com>
2018-09-05 14:31:38 -0600

    Working on making the charges hash return the correct values

--- lib/dock.rb
+++ lib/dock.rb
@@ -7,9 +7,12 @@ class Dock
     @max_rental_time = max_rental_time
     @total_rev = 0
     @rented_boats = []
+    @renter_array = []
+    @cards_charged = {}
   end
 
   def rent(boat, renter)
+    @renter_array << renter
     @rented_boats <<  boat
   end
 
@@ -18,17 +21,24 @@ class Dock
   end
 
   def log_hour
-    dock_hours = 0
-    dock_hours += 1
+    total_rev_per_renter = 0
     @rented_boats.each do |boat|
       boat.add_hour
       if boat.hours_rented <= self.max_rental_time
         @total_rev += boat.price_per_hour
+        @renter_array.each do |renter|
+          total_rev_per_renter += boat.price_per_hour
+          @cards_charged[renter.credit_card_number] = total_rev_per_renter
+        end
       end
     end
   end
 
   def revenue
     @total_rev
+  end
+
+  def charges
+    @cards_charged
   end
 end
